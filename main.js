71226ab1e53ecdddc924b119b65605dc3b6af920
saurabh <saurabh7481@gmail.com>
2022-02-23 17:13:05 +0530

    feature: fetch on reload

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
-if(localStorage.getItem('user') != null){
-    const user = JSON.parse(localStorage.getItem('user'));
-    console.log(user);
-    showData(user);
-}
-
+window.addEventListener("DOMContentLoaded", showData());
 
 const form = document.getElementById("my-form");
 
 form.addEventListener("submit", saveData);
 
 function saveData(e){
-    e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const obj = {
         name: name,
         email: email
     }
-    localStorage.setItem("user", JSON.stringify(obj));
+    localStorage.setItem("user@"+name, JSON.stringify(obj));
 }
 
-function showData(user){
-    const ul = document.getElementById("users");
-    const li = document.createElement("li");
-    li.className = "item";
-
-    li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
-    ul.appendChild(li);
+function showData(e){
+    if(localStorage.length){
+        const users = [];
+        for(let i=0; i<localStorage.length; i++){
+            const k = localStorage.key(i);
+            users.push(JSON.parse(localStorage.getItem(k)));
+        }
+        const ul = document.getElementById("users");
+        users.forEach(user => {
+            console.log(user)
+            const li = document.createElement("li");
+            li.className = "item";
+    
+            li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
+            ul.appendChild(li);
+        })
+    }
+    
 }
