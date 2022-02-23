9bf24efedc4e5870acc04ea4e4b4e1062a5c3998
saurabh <saurabh7481@gmail.com>
2022-02-23 16:44:38 +0530

    feature: user stored as object in localstorage

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+if(localStorage.getItem('user') != null){
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log(user);
+    showData(user);
+}
+
+
 const form = document.getElementById("my-form");
 
 form.addEventListener("submit", saveData);
@@ -6,6 +13,18 @@ function saveData(e){
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
+    const obj = {
+        name: name,
+        email: email
+    }
+    localStorage.setItem("user", JSON.stringify(obj));
+}
+
+function showData(user){
+    const ul = document.getElementById("users");
+    const li = document.createElement("li");
+    li.className = "item";
+
+    li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
+    ul.appendChild(li);
 }
