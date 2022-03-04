0af68856394a8a97d567e5250557c094fc118f5a
saurabh <saurabh7481@gmail.com>
2022-03-04 14:14:15 +0530

    fetching data from cloud

--- main.js
+++ main.js
@@ -22,22 +22,37 @@ function saveData(e){
  }
 
 function showData(e){
-    if(localStorage.length){
-        const users = [];
-        for(let i=0; i<localStorage.length; i++){
-            const k = localStorage.key(i);
-            users.push(JSON.parse(localStorage.getItem(k)));
-        }
-        const ul = document.getElementById("users");
-        users.forEach(user => {
-            const newUser = `<li id=${user.email}>${user.name} : ${user.email}
-                                <button onclick=deleteUser('${user.email}')> Delete</button>
-                                <button onclick=EditUser('${user.email}')> Edit</button>
-                            </li>`
+    axios.get("https://crudcrud.com/api/420fba99ab8f4462b7f8cbc31fb55d7c/appointments")
+        .then(res => {
+            const ul = document.getElementById("users");
+            res.data.forEach(el => {
+                const newUser = `<li id=${el.email}>${el.name} : ${el.email}
+                                    <button onclick=deleteUser('${el.email}')> Delete</button>
+                                    <button onclick=EditUser('${el.email}')> Edit</button>
+                                </li>`
 
-            ul.innerHTML = ul.innerHTML + newUser;
+                ul.innerHTML = ul.innerHTML + newUser;
+            })
         })
-    }
+        .catch(err => {
+            console.log(err);
+        })
+    // if(localStorage.length){
+    //     const users = [];
+    //     for(let i=0; i<localStorage.length; i++){
+    //         const k = localStorage.key(i);
+    //         users.push(JSON.parse(localStorage.getItem(k)));
+    //     }
+    //     const ul = document.getElementById("users");
+    //     users.forEach(user => {
+    //         const newUser = `<li id=${user.email}>${user.name} : ${user.email}
+    //                             <button onclick=deleteUser('${user.email}')> Delete</button>
+    //                             <button onclick=EditUser('${user.email}')> Edit</button>
+    //                         </li>`
+
+    //         ul.innerHTML = ul.innerHTML + newUser;
+    //     })
+    // }
     
 }
 
